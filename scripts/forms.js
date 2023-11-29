e9c2414a5e150136d3e9337f7930fe3a2bcb84fe
maarongross <97714828+maarongross@users.noreply.github.com>
2023-11-29 11:26:58 -0800

    almost finished part 3

--- scripts/forms.js
+++ scripts/forms.js
@@ -4,15 +4,35 @@ const errorMsgs = document.getElementsByName('error-message');
 const infoMsgs = document.getElementsByName('info-message');
 
 const nameRegExp = /[A-Za-z\s]/;
-let remainingCharacters = 300;
+
+const maxLength = comments.getAttribute('maxlength');
+const lengthOnReload = comments.value.length;
+infoMsgs[2].innerHTML = maxLength - lengthOnReload + ' characters left.';
+
+let form_errors = [];
 
 name.addEventListener('keydown', function (event) {
   if (typeof event.key !== 'string' || event.key.length !== 1) {
     return;
   }
   if (!nameRegExp.test(event.key)) {
-    errorMsgs[0].textContent = 'Please input a valid name!\n';
+    errorMsgs[0].innerHTML = 'Please input a valid name!';
     event.preventDefault();
-    errorMsgs[0].style.transition = 'all .1s ease-out';
+    setTimeout(() => {
+      errorMsgs[0].innerHTML = '';
+    }, 2000);
+  }
+}, false);
+
+comments.addEventListener('input', event => {
+  const currentLength = comments.value.length;
+  if (currentLength >= maxLength - 10) {
+    comments.style.borderColor = 'red';
+    infoMsgs[2].style.color = 'red';
+    infoMsgs[2].innerHTML = maxLength - currentLength + ' characters left!';
+  } else {
+    comments.style.borderColor = '';
+    infoMsgs[2].style.color = '';
+    infoMsgs[2].innerHTML = maxLength - currentLength + ' characters left.';
   }
 }, false);

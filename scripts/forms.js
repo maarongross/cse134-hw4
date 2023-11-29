aff188d160a328870ee260879a6eab16d73f6820
maarongross <97714828+maarongross@users.noreply.github.com>
2023-11-29 01:43:07 -0800

    added some part 3 functionality

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,1 +1,18 @@
-print(document.body)
+const name = document.getElementById('name');
+const comments = document.getElementById('usermsg');
+const errorMsgs = document.getElementsByName('error-message');
+const infoMsgs = document.getElementsByName('info-message');
+
+const nameRegExp = /[A-Za-z\s]/;
+let remainingCharacters = 300;
+
+name.addEventListener('keydown', function (event) {
+  if (typeof event.key !== 'string' || event.key.length !== 1) {
+    return;
+  }
+  if (!nameRegExp.test(event.key)) {
+    errorMsgs[0].textContent = 'Please input a valid name!\n';
+    event.preventDefault();
+    errorMsgs[0].style.transition = 'all .1s ease-out';
+  }
+}, false);
